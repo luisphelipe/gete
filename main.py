c2032af8d25dc0d3020485f23f77ce24432e2956
Luis Phelipe <luisphelipecode@gmail.com>
2017-07-20 00:29:37 -0300

    Fixed typos in main.py

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ def main():
     try:
         # Check if number is positive.
         if(int(exercise) < 1):
-            print("The parameter must be positive")
+            print("\n   The parameter must be positive")
             return     
 
     # Exception handling for when parameter isn't a number.
     except:
-        print("The parameter must be a number")
+        print("\n   The parameter must be a number")
         return
     
     # Try to get problem text from projecteuler.
@@ -31,7 +31,7 @@ def main():
     
     # Exception handling for when there isn't a problem with such parameter(parameter too high), or when and random error occur.
     except:
-        print("And error ocurred, maybe there is no exercise #{}".format(exercise))
+        print("\n   An error ocurred, maybe there is no exercise #{}".format(exercise))
 
 if __name__ == "__main__": main()
 
