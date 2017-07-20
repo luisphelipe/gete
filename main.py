a637e7f30be465d3da95164021a2074fd7ba2aa5
thales <thalesc@alunos.utfpr.edu.br>
2017-07-19 23:35:24 -0300

    code

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 
+import bs4 as bs
+import urllib.request
+import sys
+
 def main():
-    print("First commit")
+
+    exercise = sys.argv[1]
+
+    source = urllib.request.urlopen('https://projecteuler.net/problem='+ exercise)
+    site = bs.BeautifulSoup(source,'lxml')
+
+#    problemName = site.find('div', {'id': 'problem_info'})
+    problemContent = site.find('div', {'class': 'problem_conten'})
+    print(problemContent.get_text())
 
 if __name__ == "__main__": main()
 
