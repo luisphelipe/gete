20999f8285bffa555be14254ceb8fb35126289a6
Luis Phelipe <luisphelipecode@gmail.com>
2017-07-20 00:24:52 -0300

    Added commentaries to main.py

--- main.py
+++ main.py
@@ -8,14 +8,19 @@ def main():
 
     exercise = sys.argv[1]
     
+    # Try to convert parameter into integer.
     try:
+        # Check if number is positive.
         if(int(exercise) < 1):
             print("The parameter must be positive")
-            return 
+            return     
+
+    # Exception handling for when parameter isn't a number.
     except:
         print("The parameter must be a number")
         return
     
+    # Try to get problem text from projecteuler.
     try:
         source = urllib.request.urlopen('https://projecteuler.net/problem='+ exercise)
         site = bs.BeautifulSoup(source,'lxml')
@@ -23,6 +28,8 @@ def main():
         problemName = site.find('h2')
         problemContent = site.find('div', {'class': 'problem_content'})
         print("\n", problemName.get_text(), "\n \n" ,problemContent.get_text(), "\n")
+    
+    # Exception handling for when there isn't a problem with such parameter(parameter too high), or when and random error occur.
     except:
         print("And error ocurred, maybe there is no exercise #{}".format(exercise))
 
